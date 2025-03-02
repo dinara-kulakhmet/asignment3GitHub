1f5ddb63da753e58d455aa3b4d85b357d2e1050f
mac <dinara.kulakhmet@narxoz.kz>
2025-03-02 21:44:56 +0500

    Removed old file and added new file

---
+++ src/main/java/org/example/beans/EagerBean.java
@@ -0,0 +1,11 @@
+package org.example.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EagerBean {
+    public EagerBean() {
+        System.out.println("EagerBean");
+    }
+
+}
